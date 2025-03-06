5ee2866d0f62461ced364c1a56209131162c743c
Q9E <harryelliottallen@gmail.com>
2025-03-06 01:07:30 +0000

    Functional Snapping

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 /**
@@ -31,13 +33,47 @@ public class Draggable : MonoBehaviour
         transform.position = new Vector3(transform.position.x, 0, transform.position.z);
     }
 
-//Own Code:    
+    //Own Code:  
+    //When user lets go of drag:
     private void OnMouseUp()
     {
-        //Find every gameobject that is overlapping with the dropped piece
-        //Get snapping points
-        //Set the piece location
-        //Set the snapping points to not let anything else snap
+        Vector3 pieceLocation = transform.position;
 
+        float newX;
+        float newZ;
+
+        Vector3 roundedPieceLocation = new Vector3(Mathf.Round(pieceLocation.x), 0, Mathf.Round(pieceLocation.z)); //Round to integer
+
+        if (roundedPieceLocation.x % 2 == 0f) //For X Check if divisible by 2
+        {
+            newX = roundedPieceLocation.x;
+        }
+        else //Odd number
+        {
+            float lowerBound = roundedPieceLocation.x - 1;
+            float upperBound = roundedPieceLocation.x + 1;
+
+            float midPoint = lowerBound + upperBound / 2;
+
+            if (pieceLocation.x > midPoint) {newX = lowerBound;}
+            else {newX = upperBound;}
+        }
+
+        if (roundedPieceLocation.z % 2 == 0f) //For Z Check if divisible by 2
+        {
+            newZ = roundedPieceLocation.z;
+        }
+        else //Odd number
+        {
+            float lowerBound = roundedPieceLocation.z - 1;
+            float upperBound = roundedPieceLocation.z + 1;
+
+            float midPoint = lowerBound + upperBound / 2;
+
+            if (pieceLocation.z > midPoint) {newZ = lowerBound;}
+            else {newZ = upperBound;}
+        }
+
+        transform.position = new Vector3(newX, 0, newZ); //Set new coordinates
     }
 }
